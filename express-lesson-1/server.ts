a91cad6f426c4089f433229f24e08b431f4c1f33
Anna Iosif <anna@ushahidi.com>
2026-02-25 09:55:06 +0100

    Adding users-route

--- express-lesson-1/server.ts
+++ express-lesson-1/server.ts
@@ -1,6 +1,7 @@
 import express from 'express';
 
 const app = express();
+app.use(express.json());
 const PORT = 3000;
 
 app.get('/', (req, res) => {
@@ -9,4 +10,12 @@ app.get('/', (req, res) => {
 
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
+});
+
+app.get('/users', (req, res) => {
+  const users = [
+    { id: 1, name: 'John Doe' },
+    { id: 2, name: 'Jane Smith' }
+  ];
+  res.json(users);
 });
