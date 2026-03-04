de3f85d5c6856e08d3fd9849ef7757399c457827
Anna Iosif <anna@ushahidi.com>
2026-03-04 20:08:44 +0100

    Adding recipes and GET-recipes endpoint

--- recipe-box-api/server.ts
+++ recipe-box-api/server.ts
@@ -1,7 +1,45 @@
 import express from 'express';
 
+type Recipe = {
+  id: number;
+  name: string;
+  cuisine: string;
+  prepTime: number;
+};
+
 const app = express();
 const port = 3000;
+
+const recipes: Recipe[] = [
+  {
+    id: 1,
+    name: 'Smoothie',
+    cuisine: 'International',
+    prepTime: 20
+  },
+  {
+    id: 2,
+    name: 'Spaghetti Bolognese',
+    cuisine: 'Italian',
+    prepTime: 40
+  },
+  {
+    id: 3,
+    name: 'Greek Salad',
+    cuisine: 'Greek',
+    prepTime: 15
+  },
+  {
+    id: 4,
+    name: 'Palt',
+    cuisine: 'Swedish',
+    prepTime: 120
+  }
+];
+
+app.get('/recipes', (req, res) => {
+  res.json(recipes);
+});
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
