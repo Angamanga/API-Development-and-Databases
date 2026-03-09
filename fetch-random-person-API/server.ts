7c24718fdb40ab6f3c62c820da2a6c26d0e5d96c
Anna Iosif <anna@ushahidi.com>
2026-03-09 10:30:38 +0100

    Phase 3. Adding user POST route

--- fetch-random-person-API/server.ts
+++ fetch-random-person-API/server.ts
@@ -7,6 +7,16 @@ const randomPersonSchema = z.object({
   fullName: z.string().min(3).max(30),
   country: z.string().min(2).max(100),
 });
+const userSchema = z.object({
+  name: z.string().min(3).max(12),
+  age: z
+    .number()
+    .min(18, { message: "You must be at least 18 years old" })
+    .max(100, { message: "You must be less than 100 years old" })
+    .optional(),
+  email: z.email().lowercase({ message: "Email must be in lowercase" }),
+});
+
 app.use(express.json());
 
 app.get("/ping", (req, res) => {
@@ -27,6 +37,17 @@ app.get("/random-person", async (req, res) => {
     return;
   }
   res.json(validatedPerson.data);
+});
+
+app.post("/user", (req, res) => {
+  const newUser = userSchema.safeParse(req.body);
+  if (!newUser.success) {
+    res.status(400).json({ error: z.treeifyError(newUser.error) });
+    return;
+  }
+  res
+    .status(201)
+    .json({ message: "New user created successfully", user: newUser.data });
 });
 
 app.listen(port, () => {
