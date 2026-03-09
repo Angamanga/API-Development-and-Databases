829fd4fe35456700dd807a9e17165b1d25bd0e07
Anna Iosif <anna@ushahidi.com>
2026-03-09 10:13:06 +0100

    Phase 2 Fetch random person

--- fetch-random-person-API/server.ts
+++ fetch-random-person-API/server.ts
@@ -1,12 +1,33 @@
 import express from 'express';
-
+import {z } from 'zod';
 const app = express();
 const port = 3000;
 
+
+const randomPersonSchema = z.object({
+  fullName: z.string().min(3).max(30),
+  country: z.string().min(2).max(100)
+});
 app.use(express.json());
 
 app.get('/ping', (req, res) => {
   res.json({ message: 'pong' });
+});
+
+app.get('/random-person', async (req, res) => {
+  const response = await fetch('https://randomuser.me/api/');
+  const data = await response.json();
+  const randomPerson = {
+    fullName: `${data.results[0].name.first} ${data.results[0].name.last}`,
+    country: data.results[0].location.country
+  };  
+
+  const validatedPerson = randomPersonSchema.safeParse(randomPerson);
+  if (!validatedPerson.success) {
+    res.status(400).json({error: z.treeifyError(validatedPerson.error)});
+    return;
+  }
+  res.json(validatedPerson.data);
 });
 
 app.listen(port, () => {
